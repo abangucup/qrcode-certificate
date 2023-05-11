040d7c1740b76df979ed44210d8c7385fce8e784
abangucup <id.salmanmustapa@gmail.com>
2023-05-11 16:36:51 +0800

    update responsive

--- resources/views/dashboard/layouts/style.blade.php
+++ resources/views/dashboard/layouts/style.blade.php
@@ -1,3 +1,6 @@
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+<meta http-equiv="X-UA-Compatible" content="ie=edge" />
 <link href="{{ asset('assets/css/tabler.min.css') }}" rel="stylesheet" />
 <link href="{{ asset('assets/css/tabler-flags.min.css') }}" rel="stylesheet" />
 <link href="{{ asset('assets/css/tabler-payments.min.css') }}" rel="stylesheet" />
