e73bbc1edfa711a39f378da9fcca0efb27762b5b
abangucup <id.salmanmustapa@gmail.com>
2023-05-12 14:17:57 +0800

    redesign qrcode

--- resources/views/dashboard/hasil/index.blade.php
+++ resources/views/dashboard/hasil/index.blade.php
@@ -44,49 +44,52 @@
         <div class="row row-cards">
 
             @foreach ($usahas as $usaha)
-            <div class="col-lg-6 mt-4">
-                <div class="card  shadow-sm  print-card-wrapper">
-                    <div class="row no-gutters">
-                        <div class="col-md-3 d-flex justify-content-center mt-2">
-                            {!! QrCode::size(110)->generate(route('usaha.show', $usaha->id)); !!}
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h4 class="card-title">{{ $usaha->pemilik_usaha }}</h4>
-                                <ul class="list-unstyled space-y-1">
-                                    <li>
-                                        <!-- Download SVG icon from http://tabler-icons.io/i/check -->
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24"
-                                            height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-                                            fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path d="M5 12l5 5l10 -10" />
-                                        </svg>
-                                        Lulus Uji Layak Sehat
-                                    </li>
-                                    <li>
-                                        <!-- Download SVG icon from http://tabler-icons.io/i/check -->
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24"
-                                            height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-                                            fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path d="M5 12l5 5l10 -10" />
-                                        </svg>
-                                        Lulus Kesehatan Karyawan
-                                    </li>
-                                    <li>
-                                        <!-- Download SVG icon from http://tabler-icons.io/i/check -->
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24"
-                                            height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-                                            fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path d="M5 12l5 5l10 -10" />
-                                        </svg>
-                                        Lulus Masa Berlaku Sertifikat Usaha
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="card shadow-sm print-card-wrapper">
+                    <!-- Photo -->
+                    <div class=" d-flex justify-content-center mt-2">
+                        <img src="{{ asset('assets/static/pemkot.png') }}" class="m-2" width="50" alt="pemkot">
+                        <img src="{{ asset('assets/static/kemkes.png') }}" class="m-2 float-right" width="100"
+                            alt="kemkes">
+                    </div>
+
+                    <div class="col-md-12 d-flex justify-content-center">
+                        {!! QrCode::size(300)->generate(route('usaha.show', $usaha->id)); !!}
+                    </div>
+                    <div class="card-body">
+                        <h2 class="mt-2">{{ $usaha->pemilik_usaha }}</h2>
+                        <ul class="list-unstyled space-y-1">
+                            <li>
+                                <!-- Download SVG icon from http://tabler-icons.io/i/check -->
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24"
+                                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                    stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M5 12l5 5l10 -10" />
+                                </svg>
+                                Lulus Uji Layak Sehat
+                            </li>
+                            <li>
+                                <!-- Download SVG icon from http://tabler-icons.io/i/check -->
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24"
+                                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                    stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M5 12l5 5l10 -10" />
+                                </svg>
+                                Lulus Kesehatan Karyawan
+                            </li>
+                            <li>
+                                <!-- Download SVG icon from http://tabler-icons.io/i/check -->
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon text-green" width="24" height="24"
+                                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                    stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M5 12l5 5l10 -10" />
+                                </svg>
+                                Lulus Masa Berlaku Sertifikat Usaha
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -103,6 +106,7 @@
         /* Set lebar kertas */
         @page {
             size: A4 landscape;
+            /* size: A4 potrait; */
             /* Atur ukuran kertas */
             margin: 0, 10px, 0, 10px;
         }
